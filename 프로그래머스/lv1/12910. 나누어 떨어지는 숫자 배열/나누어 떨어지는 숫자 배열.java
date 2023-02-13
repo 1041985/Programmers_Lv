5fdf4aa261c52ff9846ce7e3811202770ebe993a
minha <113270305+1041985@users.noreply.github.com>
2023-02-14 00:46:21 +0900

    Update 나누어 떨어지는 숫자 배열.java

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -3,26 +3,26 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int divisor) {
         
-        int count = 0;
-        int number = 0;
+        int cnt = 0;
+        int num = 0;
         
         for(int i = 0; i < arr.length; i++){
             if(arr[i] % divisor == 0){
-                count++;
+                cnt++;
             }
         }
         
-        if(count == 0){
+        if(cnt == 0){
             int[] answer = {-1};
             return answer;
         }
         
-        int[] answer = new int[count];
+        int[] answer = new int[cnt];
         
         for(int i = 0; i < arr.length; i++){
             if(arr[i] % divisor == 0){
-                answer[number] = arr[i];
-                number++;
+                answer[num] = arr[i];
+                num++;
             }
         }
         
