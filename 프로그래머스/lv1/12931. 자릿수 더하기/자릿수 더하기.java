d89e662fe27a7d21585fd449ed05aa6067e4851e
minha <113270305+1041985@users.noreply.github.com>
2023-02-26 21:06:29 +0900

    Update 자릿수 더하기.java

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,10 +4,10 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        String s = Integer.toString(n); //int n을 String으로 변환
+        String str = Integer.toString(n);
         
-        for(int i=0; i<s.length(); i++){
-            answer += Integer.parseInt(s.substring(i, i+1));
+        for(int i=0; i<str.length(); i++){
+            answer += Integer.parseInt(str.substring(i, i+1));
         }
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("Hello Java");
