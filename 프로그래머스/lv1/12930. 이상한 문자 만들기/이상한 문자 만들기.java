53b75e33a77c136b9bfef42e49956b8c92b555fc
minha <113270305+1041985@users.noreply.github.com>
2023-02-26 20:37:32 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 5.22 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,13 +1,22 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        int cnt = 0;
-        String[] array = s.split("");
-
-        for(String ss : array) {
-            cnt = ss.contains(" ") ? 0 : cnt + 1;
-            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
-        }
+        String[] str = s.split("");
+        
+        int idx = 0;
+        for(int i=0; i<str.length; i++) {
+            if(str[i].equals(" ")) {
+                idx = 0;
+            } else if(idx % 2 == 0) {
+                str[i] = str[i].toUpperCase();
+                idx++;
+            } else if(idx % 2 != 0) {
+                str[i] = str[i].toLowerCase();
+                idx++;
+            }
+            answer += str[i];
+        } 
+        
         return answer;
     }
 }
