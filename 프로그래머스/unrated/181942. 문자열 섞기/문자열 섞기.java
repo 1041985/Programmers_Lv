b7b31005de2ed387f54535f9dd6a5d0c93a3b1f6
minha <113270305+1041985@users.noreply.github.com>
2023-07-25 12:25:16 +0900

    Update 문자열 섞기.java

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -4,6 +4,10 @@ class Solution {
         for(int i=0;i<str1.length();i++){
             answer += str1.substring(i,i+1);
             answer += str2.substring(i,i+1);
+            // 1.00ms 더 빠름
+            
+            //answer += "" + str1.charAt(i) + str2.charAt(i);
+            // 10.00ms 넘어감
         }
         return answer;
     }
